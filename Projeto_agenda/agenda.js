03b4b9ec633f8b74b92db5077e2ea53ed497ec22
ARTHUR GON�ALVES POLO <25170150@educ123.sp.senai.br>
2025-05-07 11:15:25 -0300

    Add files

--- Projeto_agenda/agenda.js
+++ Projeto_agenda/agenda.js
@@ -2,48 +2,111 @@
 
 const readline = require('readline-sync');
 
-let agenda = [];
+const CLS = '\x1Bc';
+
+const agenda = [];
+
+const fs = require('fs');
+
+const contatos = {};
 
 function menu() {
-    console.log(`\n* Projeto Agenda *\n`);
-    console.log(`1. Adicionar registro `);
-    console.log(`2. listar agenda`);
-    console.log(`3. Buscar usuário`);
-    console.log(`4. Apagar registro`);
-    console.log(`5. Salvar em disco`);
-    console.log(`6. Sair\n`);
+    console.log(`** Agenda **\n`);
+    console.log(`1. Cadastrar usuários`);
+    console.log(`2. Mostrar dados de um usuário`);
+    console.log(`3. Listar todos nomes cadastrados`);
+    console.log(`4. Remover usuário`);
+    console.log(`5. Limpar toda agenda`);
+    console.log(`6. Gravar arquivo`);
+    console.log(`7. ler arquivo`);
+    console.log(`8. Sair\n`)
     const opção = Number(readline.question(`Opcao: `));
     console.log();
     return opção;
 }
 
 function adicionar() {
-    console.log(`* Tela adicionar *\n`)
     let nome = readline.question(`Nome: `);
-    let endereço = readline.question(`Endereco: `);
+    let email = readline.question(`E-mail: `);
     let telefone =readline.question(`Telefone: `);
-    return {nome, endereço, telefone};
+    return {nome, email, telefone};
+}
+
+function gravar_agenda() {
+    const file = 'contatos.json';
+    const conteudoGravar = JSON.stringify(contatos);
+    fs.writeFileSync(file, conteudoGravar);
 }
 
 let opcao;
 do {
     opcao = menu();
     if (opcao === 1) {
+        console.log(`${CLS}`);
+        console.log(`** Cadastrar usuário **\n`);
         let registro = adicionar();
         agenda.push(registro);
-        console.log(`\nRegistro adicionado com sucesso!`);
-    } else if (opcao === 2) {
-        console.log(`* Lista de registros *`);
-        console.log(agenda);
-    } else if (opcao === 3) {
-        console.log(`* Buscar usuário *`);
-    } else if (opcao === 4) {
-        console.log(`* Apagar registro *`);
-    } else if (opcao === 5) {
-        console.log(`* Salvar em disco *`);
-    } else if (opcao === 6) {
-        console.log(`* Fechando agenda... *`);
+        console.log(`${CLS}`);
+    } 
+    
+    else if (opcao === 2) {
+        console.log(`${CLS}`);
+        console.log(`** Mostrar dados de um usuário **\n`);
+        const nome = readline.question(`Infome o nome do usuário para o fornecimentos dos dados: `);
+        console.log();
+        const usuario = agenda.find(user => user.nome === nome);
+        if (usuario) {
+            console.log(`Nome: ${usuario.nome}`);
+            console.log(`Email: ${usuario.email}`);
+            console.log(`Telefone: ${usuario.telefone}\n`)
+        } else {
+            console.log(`Usuário não encontrado. \n`);
+        }
+    } 
+    
+    else if (opcao === 3) {
+        console.log(`${CLS}`);
+        console.log(`** Nomes cadastrados **\n`);
+        const nomes = agenda.map(contato => contato.nome);
+        console.log(nomes,'\n');
+    } 
+    
+    else if (opcao === 4) {
+        console.log(`** Remover usuário **\n`);
+        const nome = readline.question(`Insira o nome do usuário que deseja remover: `);
+        console.log();
+        const usuario = agenda.find(user => user.nome === nome);
+        if (usuario) {
+            agenda.splice(0, 1);
+        } else {
+            console.log(`Usuário não encontrado. \n`);
+        }
+    } 
+    
+    else if (opcao === 5) {
+        console.log(`${CLS}`);
+        console.log(`** Limpar toda agenda **\n`);
+        const limpar = readline.question(`Deseja realmente limpar toda a agenda s/n ? `);
+        console.log();
+        if (limpar == 's') {
+            
+        } else {}
+    } 
+    
+    else if (opcao === 6) {
+        console.log(`** Gravar arquivo **\n`);
+        
+    } 
+    
+    else if (opcao === 7) {
+        console.log(`** ler arquivo **\n`);
+    } 
+    
+    else if (opcao === 8) {
+        console.log(`${CLS}`);
+        console.log(`** Fechando agenda **\n`);
     } else {
-        console.log(`* Opção inválida. Tente novamente. *`);
+        console.log(`${CLS}`);
+        console.log(`** Opção inválida tente novamente **\n`);
     }
-} while (opcao != 6);
+} while (opcao != 8);
